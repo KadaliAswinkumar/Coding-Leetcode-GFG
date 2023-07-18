00c41051e57bb43de95c8dcd5be4a513f9854d70
Kadali Aswin Kumar <89196595+KadaliAswinkumar@users.noreply.github.com>
2023-07-18 21:13:36 +0530

    Time: 32 ms (78.49%), Space: 25.5 MB (28.08%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        auto n = nums.size();
+        vector<int> temp(n);
+        for (int i = n - 1; i >= 0; i--) {
+            temp[(i+k) % n] = nums[i];
+        }
+        swap(nums, temp);
+    }
+};
