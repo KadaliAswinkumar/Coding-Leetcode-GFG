04984f7a32daaeb86ec9d017888ac28c786bb0c5
Kadali Aswin Kumar <89196595+KadaliAswinkumar@users.noreply.github.com>
2023-07-18 21:23:23 +0530

    Time: 19 ms (93.94%), Space: 19.3 MB (30.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+    int pos=0;
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(nums[pos]==0 && nums[i]!=0)
+        {
+            swap(nums[pos],nums[i]);
+            pos++;
+        }
+        if(nums[pos]!=0)
+        {
+            pos++;
+        }
+    }
+    }
+};
